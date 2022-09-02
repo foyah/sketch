1bc830aab06c40adddfd745a9ca9b06b2a97c8be
foyah <yvessomba@gmail.com>
2022-09-02 17:22:52 +0200

    Rajout JS

---
+++ script.js
@@ -0,0 +1,29 @@
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+function setCurrentColor(newColor){
+    currentColor = newColor
+}
+
+function setCurrentMode(newMode){
+    activateButton(newMode)
+    currentMode = newMode
+}
+
+function setCurrentSize(newSize){
+    currentSize = newSize
+}
+
+const colorPicker = document.getElementById('colorPicker')
+const colorBtn = document.getElementById('colorBtn')
+const rainbowBtn = document.getElementById('rainbowBtn')
+const eraserBtn = document.getElementById('eraserBtn')
+const clearBtn = document.getElementById('clearBtn')
+const sizeValue = document.getElementById('sizeValue')
+const sizeSlider = document.getElementById('sizeSlider')
+const grid = document.getElementById('grid')
